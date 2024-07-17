c93c393e70a3a94991b5125475d2c75ba33df895
noemipeteilh <169151883+noemipeteilh@users.noreply.github.com>
2024-07-17 13:12:38 +0200

    Agregar scraper

---
+++ src/wikipedia_scraper.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+class Wikipedia_scraper:
+    def __init__(self, url, input):
+        self.url = url
+        self.input = input
+
+    def scraper (self):
+        try:
+            response = requests.get(f"{self.url}{self.input}")
+            soup = BeautifulSoup(response.text,'html.parser')
+            title = soup.find("h1").text
+            mw_content = soup.find(id="mw-content-text")
+            if mw_content.find(id="noarticletext"):
+                raise Exception("No se ha encontrado el árticulo seleccionado")
+
+            elif mw_content.find("div", class_="mw-disambig-page"):
+
+                raise Exception("Se ha encontrado una página de desambiguación")
+
+            else:
+                texts = mw_content.find_all("p",limit=5)
+                text = ""
+            for p in texts:
+                t = p.get_text()
+                text = text + t
+
+            return title, text
+        except Exception as e:
+            print(e)
+
