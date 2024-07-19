259cddf85dcc045aab55ca7240b2926d44dc7771
Erieltxu <ld.aldecoa@gmail.com>
2024-07-19 11:28:09 +0200

    test translator agregado

---
+++ tests/test_translator.py
@@ -0,0 +1,13 @@
+import pytest
+from src.translator import Translator
+
+
+def test_translate_success():
+    input_text = "Hola"
+    input_tgt_lang = "en"
+    input_src_lang = "es"
+
+    my_translator = Translator(input_text, input_tgt_lang, input_src_lang)
+    result = my_translator.translate()
+
+    assert result == "Hello"
